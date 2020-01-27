23a5bbd557ac3811fbd3397f3dce9832d09dc587
Louca Coles <loucacoles.aprioridigital.com>
2020-01-27 08:28:37 +0000

    Initial Project

---
+++ GGJ2020Unity/Assets/Classes/Generic/Timer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Timer
+{
+    private float duration;
+    private float timer;
+
+    private bool resetOnFinish;
+
+    public event Action OnTimerEnd;
+
+    public Timer(float _duration, bool _resetOnFinish)
+    {
+        duration = _duration;
+        timer = duration;
+        resetOnFinish = _resetOnFinish;
+    }
+
+    public void Tick(float _deltaTime)
+    {
+        if (timer == 0) { return; }
+
+        timer -= _deltaTime;
+
+        // If timer is finished
+        if (isTimerFinished())
+        {
+            // If there are any listeners then invoke the event
+            OnTimerEnd?.Invoke();
+        }
+    }
+
+    private bool isTimerFinished()
+    {
+        if (timer < 0)
+        {
+            if (resetOnFinish)
+            {
+                timer = duration;
+            }
+            else
+            {
+                timer = 0;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ GGJ2020Unity/Assets/Classes/Singletons/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+    protected virtual void CreateInstance()
+    {
+        if (instance != null && instance != this)
+        {
+            Debug.LogError("Instance already exists");
+            Destroy(this.gameObject);
+            return;
+        }
+    }
+}
