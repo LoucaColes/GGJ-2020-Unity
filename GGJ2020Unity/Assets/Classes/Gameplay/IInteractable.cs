cd14ae807c5219014fa8b6e18de5abbfc3bca7a4
LoucaColes_ <coleslouca@gmail.com>
2020-02-01 14:54:39 +0000

    Added a break function to the Interactable interface. Added a base class for the cardboard prop. Added Jack's stuff to the project.

--- GGJ2020Unity/Assets/Classes/Gameplay/IInteractable.cs
+++ GGJ2020Unity/Assets/Classes/Gameplay/IInteractable.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public interface IInteractable
 {
+    void Break();
     void Interact();
 }
---
+++ GGJ2020Unity/Assets/Classes/Gameplay/Interactables/CardboardProp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardboardProp : MonoBehaviour, IInteractable
+{
+    private Vector3 collapseRotation = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Break()
+    {
+
+    }
+
+    public void Interact()
+    {
+
+    }
+}
