8ad88511251aa4cb636b0ced40ad78cc55747b9f
instanceerror <sparseoffering@gmail.com>
2020-02-01 17:19:18 +0000

    Set up environment; added basic light

---
+++ GGJ2020Unity/Assets/Classes/Camera/PlayCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform stageTarget;
+
+    [SerializeField]
+    private Transform playerTarget;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(playerTarget);
+        transform.position = playerTarget.transform.position + offset;
+    }
+}
