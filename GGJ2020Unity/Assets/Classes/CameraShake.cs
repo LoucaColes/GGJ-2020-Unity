b1f5c0d0752d43126e059d313b31a0fd19b692bd
LoucaColes_ <coleslouca@gmail.com>
2020-02-01 10:26:16 +0000

    Created a quick camera shake

---
+++ GGJ2020Unity/Assets/Classes/CameraShake.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraShake : Singleton<CameraShake>
+{
+    [SerializeField] private DOTweenAnimation cameraShake = null;
+
+    private void Awake()
+    {
+        CreateInstance();
+
+        instance = this;
+    }
+
+    public void Shake()
+    {
+        cameraShake.DORestart();
+        cameraShake.DOPlay();
+    }
+}
