04da5794150b089ae617031165e3c7950cf72240
instanceerror <sparseoffering@gmail.com>
2020-02-02 12:45:13 +0000

    Randomiser; Material changer; change to ui

---
+++ GGJ2020Unity/Assets/Classes/Generic/RandomNumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNumber : Singleton<RandomNumber>
+{
+    private System.Random random = new System.Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateInstance();
+        instance = this;
+    }
+
+    public int GetRandomValue()
+    {
+        return random.Next(0, 4);
+    }
+}
